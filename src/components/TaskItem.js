c5302146913c22d4061ba233a016b93a3dd2508a
HungLQ <hunglq@rikkeisoft.com>
2019-02-11 11:58:29 +0700

    training redux

--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -27,7 +27,6 @@ class TaskItem extends Component {
           <button
             type="button"
             className="btn btn-warning"
-            onClick={}
           >
             <span className="fa fa-pencil mr-5"></span>Edit
           </button>
